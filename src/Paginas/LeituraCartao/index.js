a5566fb66dcb1d749bfaaab80a5882766175a57d
Marcelo Damasceno <marcelodamasceno@gmail.com>
2023-04-24 13:52:42 -0300

    adiciona validacao de cartao

--- src/Paginas/LeituraCartao/index.js
+++ src/Paginas/LeituraCartao/index.js
@@ -2,16 +2,33 @@ import Botao from "componentes/Botao";
 import CampoTexto from "componentes/CampoTexto";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { validCartaoNFC } from "utils/Regex";
+import './LeituraCartao.css';
 
 export default function LeituraCartao() {
-  const [cartao, setCartao] = useState('123456789')
+  const [cartao, setCartao] = useState('12345678')
+  const [cartaoErr, setCartaoErr] = useState(false)
   const navegar = useNavigate()
 
-  function cadastrarCartao() {
-    let dadosjson = JSON.stringify({ cartao });
-    // post('https://httpbin.org/post', dadosjson)
-    navegarPara('/fim')
+  function validaCartao() {
+    console.log('00' + cartao)
+    if (!validCartaoNFC.test('00' + cartao)) {
+      setCartaoErr(true)
+      return false
+    }
+    else {
+      setCartaoErr(false)
+      return true
+    }
   }
+  function cadastrarCartao() {
+    if (validaCartao()) {
+      let dadosjson = JSON.stringify({ cartao: '00' + cartao });
+      // post('https://httpbin.org/post', dadosjson)
+      navegarPara('/fim')
+    }
+  }
+
   function navegarPara(path) {
     navegar(path)
   }
@@ -33,6 +50,7 @@ export default function LeituraCartao() {
         <Botao id='botao' onClick={() => cadastrarCartao()} >
           Cadastrar
         </Botao>
+        {cartaoErr && <div className="mensagem-erro"> <p>Seu cartão é inválido</p> <p>Insira novamente seu cartão!</p></div>}
       </div>
     </section >
   )
--- src/utils/Regex.jsx
+++ src/utils/Regex.jsx
@@ -4,3 +4,4 @@ export const validEmail = new RegExp(
 export const validPassword = new RegExp("^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$");
 
 export const validMatricula = new RegExp("^([0-9]{6}|[0-9]{13})$");
+export const validCartaoNFC = new RegExp("^[0-9]{10}$");
