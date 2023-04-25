8a42594aad85e88c6e9e663626fb0f6d85991823
Marcelo Damasceno <marcelodamasceno@gmail.com>
2023-04-25 16:37:49 -0300

    adiciona opcao de nocors

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,18 +1,24 @@
-async function post(url, dadosjson) {
-
-  const response = await fetch(url, {
+async function post(url, dadosjson, nocors) {
+  let headerOptions = {
     method: 'post',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: dadosjson,
-  })
+    body: dadosjson
+  }
+  if (nocors) {
+    headerOptions.headers.mode = 'no-cors'
+  }
+  console.log(headerOptions)
+  const response = await fetch(url, headerOptions)
+  const resposta = response.json()
   if (!response.ok) {
-    return false;
+    return { 'status': false, 'msg': resposta };
   } else {
     console.log("post realizado com sucesso")
-    return true;
+    return { 'status': true, 'msg': resposta };
   }
 }
 
-export { post };
+export { post }
+
