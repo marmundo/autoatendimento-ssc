ffab3244164415f65b8bb997b166930ca63017da
Marcelo Damasceno <marcelodamasceno@gmail.com>
2023-04-27 14:00:34 -0300

    compativel com cartao de professor e aluno

--- src/Paginas/LeituraCartao/index.js
+++ src/Paginas/LeituraCartao/index.js
@@ -3,15 +3,22 @@ import CampoTexto from "componentes/CampoTexto";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { validCartaoNFC } from "utils/Regex";
+import { post } from "utils/utils";
 import './LeituraCartao.css';
 
-export default function LeituraCartao() {
-  const [cartao, setCartao] = useState('12345678')
+export default function LeituraCartao({ usuario }) {
+  const [cartao, setCartao] = useState('')
   const [cartaoErr, setCartaoErr] = useState(false)
   const navegar = useNavigate()
 
+  const url = 'https://10.230.0.46/api/v2/autoatendimento'
+
+
   function validaCartao() {
-    if (!validCartaoNFC.test('00' + cartao)) {
+    if (cartao.length === 8) {
+      setCartao('00' + cartao)
+    }
+    if (!validCartaoNFC.test(cartao)) {
       setCartaoErr(true)
       return false
     }
@@ -20,11 +27,37 @@ export default function LeituraCartao() {
       return true
     }
   }
+
+  function montaPayload(usuario, cartao) {
+    let payload = {}
+    payload.nome = usuario.nome
+    payload.registro = usuario.matricula
+    payload.tag = cartao
+    payload.token = usuario.token
+    payload.vinculo = true
+    return JSON.stringify(payload)
+  }
+
+  async function cadastrarUsuario(usuario) {
+    let cartao = cadastrarCartao()
+    let payload = montaPayload(usuario, cartao.cartao)
+    let resposta = await post(url, payload)
+    if (resposta.status) {
+      const titulo = "Obrigado!!!"
+      const subtitulo = "Seu cartão foi cadastrado com sucesso!"
+      navegarPara(`/mensagem?titulo=${titulo}&subtitulo=${subtitulo}`)
+    } else {
+      const titulo = "ERRO!!!";
+      navegar(`/mensagem?titulo=${titulo}&subtitulo=${resposta.msg}`)
+    }
+  }
+
   function cadastrarCartao() {
     if (validaCartao()) {
-      let dadosjson = JSON.stringify({ cartao: '00' + cartao });
-      // post('https://httpbin.org/post', dadosjson)
-      navegarPara('/fim')
+      if (cartao.length === 8) {
+        return { cartao: '00' + cartao };
+      }
+      return { cartao: cartao };
     }
   }
 
@@ -43,10 +76,10 @@ export default function LeituraCartao() {
           autoFocus
           onKeyUp={(e) => {
             if (e.key === "Enter") {
-              cadastrarCartao();
+              cadastrarUsuario(usuario)
             }
           }} />
-        <Botao id='botao' onClick={() => cadastrarCartao()} >
+        <Botao id='botao' onClick={() => cadastrarUsuario(usuario)} >
           Cadastrar
         </Botao>
         {cartaoErr && <div className="mensagem-erro"> <p>Seu cartão é inválido</p> <p>Insira novamente seu cartão!</p></div>}
