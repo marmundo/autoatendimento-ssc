9e602fec96ba5f28e4f1a8648ebd2520fba46a7e
Marcelo Damasceno <marcelodamasceno@gmail.com>
2023-05-16 22:54:19 -0300

    adiciona pagina nao encontrada

---
+++ src/Paginas/NaoEncontrada/index.js
@@ -0,0 +1,9 @@
+import './NaoEncontrada.css';
+export default function NaoEncontrada() {
+  return (
+    <>
+      <h2>Ops!</h2>
+      <p>O conteúdo que você procura não foi encontrado!</p>
+    </>
+  )
+}
